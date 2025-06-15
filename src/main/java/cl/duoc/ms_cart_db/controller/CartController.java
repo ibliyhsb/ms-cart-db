eb85d1d01863411ce0ea7c2d2c23bfc57b8f4d64
ibliyhsb <catd.rojas@duocuc.cl>
2025-06-14 23:45:55 -0400

    ms-cart-db

--- src/main/java/cl/duoc/ms_cart_db/controller/CartController.java
+++ src/main/java/cl/duoc/ms_cart_db/controller/CartController.java
@@ -5,9 +5,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import cl.duoc.ms_cart_db.model.dto.CartDTO;
+import cl.duoc.ms_cart_db.model.dto.ProductDTO;
 import cl.duoc.ms_cart_db.service.CartService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 @RestController
@@ -18,10 +25,24 @@ public class CartController {
     CartService cartService;
 
     @GetMapping("/getCartById/{idCart}")
-    public ResponseEntity<?> getCartById(@PathVariable("idCart" Long idCart)) {
+    public ResponseEntity<?> getCartById(@PathVariable("idCart") Long idCart) {
         return cartService.getCartById(idCart);
     }
     
+    @PostMapping("/insertCart")
+    public ResponseEntity<String> insertCart (CartDTO cartDTO){
+        return cartService.insertCart(cartDTO);
+    }
+    
+    @PutMapping("/insertProduct/{idCart}")
+    public ResponseEntity<?> insertProduct (ProductDTO productDTO, @PathVariable("idCart") Long idCart){
+        return cartService.insertProduct(productDTO, idCart);
+    }
+
+    @DeleteMapping("/deleteProduct/{idCart}")
+    public ResponseEntity<?> deleteProduct(String productName, Long idCart){
+        return cartService.deleteProduct(productName, idCart);
+    }
 
 
 }
